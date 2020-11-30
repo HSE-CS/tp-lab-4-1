d07909c7972f13e99a775d5685647c428c82ef45
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-12-01 00:58:27 +0300

    added all declarations in class

--- include/Automata.h
+++ include/Automata.h
@@ -1,30 +1,39 @@
 #ifndef AUTOMATA
 #define AUTOMATA
 #include <string>
+#include <map>
 
 enum class STATES {
-	OFF = 0, WAIT, ACCEPT, CHECK, COOK
+  OFF = 0, WAIT, ACCEPT, CHECK, COOK
 };
 
-class Automata
-{
-private:
-	STATES state;
-	int cash;
-	int prices[50]; //?
-
-public:
-	Automata();
-	~Automata();
-	void on();
-	void off();
-	void coin();
-	void getMenu();
-	void getState();
-	void choice();
-	void check();
-	void cancel();
-	void cook();
-	void finish();
+class Automata {
+ private:
+  size_t all_income = 0;
+  STATES state = STATES::OFF;
+  std::map<std::string, size_t> menu { 
+  {"water", 20}, 
+  {"tea", 30}, 
+  {"coffee", 40}, 
+  {"cocoa", 35},
+  {"hot chocolate", 50},
+  {"latte", 60} 
+  };
+  void cook(std::string);
+  void check(std::string);
+  void finish();
+  size_t cash = 0;
+  unsigned short sugar = 0;
+ public:
+  Automata();
+  void on();
+  void off();
+  void coin(int);
+  const std::map<std::string, size_t>& getMenu();
+  const STATES getState();
+  const size_t getBalance();
+  void choice(std::string, unsigned short);
+  void cancel();
 };
+void printMenu(std::map<std::string, size_t> menu);
 #endif
