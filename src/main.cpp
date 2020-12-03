4fd482ebe23b63f82a431d0150671f08d667750c
valvarl <valvarl@ya.ru>
2020-12-03 17:32:08 +0300

    initial

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by freeb on 23.11.2020.
+//
+
+#include <string>
+#include <iostream>
+#include "Automata.h"
+
+using namespace std;
+
+int main () {
+/*    string menu[]{"coca cola", "sprite", "fanta"};
+    double price[]{55, 59.90, 50};
+    Automata automata{menu, price};
+
+    cout << STATES(automata.getState()) << endl;
+    automata.on();
+    cout << STATES(automata.getState()) << endl;
+
+    automata.coin(30);
+    cout << STATES(automata.getState()) << endl;
+    automata.coin(10);
+    cout << STATES(automata.getState()) << endl;
+
+    automata.choice(1);
+    cout << STATES(automata.getState()) << " " << automata.getCash() << endl;
+
+    automata.coin(20);
+    automata.choice(1);
+    cout << STATES(automata.getState()) << " " << automata.getCash() << endl;
+
+    automata.off();
+    cout << STATES(automata.getState()) << endl;*/
+}
