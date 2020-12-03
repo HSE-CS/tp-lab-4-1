b20696111861e0e642e9c508fa1fddadad507770
Смирнов Григорий <greg.smirnov2013@yandex.ru>
2020-12-03 17:16:06 +0300

    more complex tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -30,7 +30,7 @@ TEST(test_cancel, test4)
     EXPECT_EQ(3, A.cancel());
 }
 
-TEST(test_cancel2, test5)
+TEST(test_cancel, test5)
 {
     Automata A;
     A.on();
@@ -52,6 +52,61 @@ TEST(test_drink, test7)
     A.coin(13);
     EXPECT_EQ(3, A.choice("hot water"));
 }
+
+TEST(test_drink, test8)
+{
+  Automata A;
+  A.on();
+  A.coin(15);
+  A.coin(50);
+  EXPECT_EQ(30, A.choice("americano"));
+  EXPECT_TRUE(A.takeDrink() == "americano");
+}
+
+TEST(test_drink, test9)
+{
+  Automata A;
+  A.on();
+  A.coin(15);
+  A.coin(50);
+  EXPECT_EQ(25, A.choice("hot chocolate"));
+  EXPECT_TRUE(A.takeDrink() == "hot chocolate");
+  EXPECT_EQ(WAIT, A.getState());
+}
+
+TEST(test_no_drink, test10)
+{
+  Automata A;
+  A.on();
+  A.coin(15);
+  A.coin(50);
+  EXPECT_EQ(43, A.choice("milk"));
+  EXPECT_TRUE(A.takeDrink() == "milk");
+  EXPECT_EQ(WAIT, A.getState());
+  EXPECT_TRUE(A.takeDrink() == "None");
+}
+
+TEST(test_no_drink, test11)
+{
+  Automata A;
+  EXPECT_TRUE(A.takeDrink() == "None");
+  EXPECT_EQ(OFF, A.getState());
+}
+
+TEST(test_no_drink, test12)
+{
+  Automata A;
+  A.on();
+  A.coin(15);
+  A.coin(33);
+  EXPECT_EQ(48, A.cancel());
+  EXPECT_EQ(WAIT, A.getState());
+  EXPECT_TRUE(A.takeDrink() == "None");
+}
+
+
+
+
 
 
 
