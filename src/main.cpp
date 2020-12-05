c8250b22d5d49d5f367ea371be946acc89697002
Nikita Kasyanov <kasyanikita@gmail.com>
2020-12-05 10:02:04 +0300

    First attempt

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Automata.h"
+
+int main() {
+    Automata a1;
+    auto drinks = a1.getMenu();
+    for (const auto &item: drinks) {
+        std::cout << item.first << " - " << item.second << '\n';
+    }
+    std::cout << a1.getState() << '\n';
+    a1.on();
+    std::cout << a1.getState() << '\n';
+    a1.coin(10);
+    std::cout << a1.getState() << '\n';
+    a1.coin(20);
+    std::cout << a1.getBalance() << '\n';
+    a1.choice("milk");
+    std::cout << a1.getState() << '\n';
+    a1.check();
+    a1.cook();
+    std::cout << a1.getState() << '\n';
+    a1.coin(70);
+    a1.choice("milk");
+    std::cout << a1.getState() << '\n';
+    a1.check();
+    a1.cook();
+    a1.finish();
+    a1.off();
+    std::cout << a1.getState() << '\n';
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,125 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(automata, test1) {
+    Automata a;
+    std::string expected = "OFF";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test2) {
+    Automata a;
+    a.on();
+    std::string expected = "WAIT";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test3) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    std::string expected = "ACCEPT";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test4) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.coin(20);
+    int expected = 70;
+    EXPECT_EQ(expected, a.getBalance());
+}
+
+TEST(automata, test5) {
+    Automata a;
+    std::unordered_map<std::string, int> menu = {
+            {"coffee", 200}, {"tea", 150},
+            {"milk", 100},{"water", 50},
+            {"cola", 175}};
+    EXPECT_EQ(menu, a.getMenu());
+}
+
+TEST(automata, test6) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    a.cancel();
+    std::string expected = "WAIT";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test7) {
+    Automata a;
+    a.on();
+    a.coin(200);
+    a.choice("coffee");
+    std::string expected = "CHECK";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test8) {
+    Automata a;
+    a.on();
+    a.coin(200);
+    a.choice("coffee");
+    a.cancel();
+    std::string expected = "WAIT";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test9) {
+    Automata a;
+    a.on();
+    a.coin(100);
+    a.choice("coffee");
+    a.check();
+    std::string expected = "ACCEPT";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test10) {
+    Automata a;
+    a.on();
+    a.coin(200);
+    a.choice("coffee");
+    a.check();
+    std::string expected = "CHECK";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test11) {
+    Automata a;
+    a.on();
+    a.coin(200);
+    a.choice("coffee");
+    a.check();
+    a.cook();
+    std::string expected = "COOK";
+    EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test12) {
+    Automata a;
+    a.on();
+    a.coin(200);
+    a.choice("coffee");
+    a.check();
+    a.cook();
+    a.finish();
+    std::string expected = "WAIT";
+EXPECT_EQ(expected, a.getState());
+}
+
+TEST(automata, test13) {
+    Automata a;
+    a.on();
+    a.coin(200);
+    a.choice("coffee");
+    a.check();
+    a.cook();
+    a.finish();
+    a.off();
+    std::string expected = "OFF";
+    EXPECT_EQ(expected, a.getState());
+}
