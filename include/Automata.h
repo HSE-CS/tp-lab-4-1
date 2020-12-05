326e97ffd0554ebc33c2dcb1c30569146bbfc649
Nikita Kasyanov <kasyanikita@gmail.com>
2020-12-05 12:12:26 +0300

    Initial program 2

--- include/Automata.h
+++ include/Automata.h
@@ -1,26 +1,28 @@
 #ifndef AUTOMATA_AUTOMATA_H
 #define AUTOMATA_AUTOMATA_H
 
-#include <map>
+#include <unordered_map>
 #include <string>
+#include <fstream>
 #include <vector>
 
 enum STATES {
     OFF, WAIT, ACCEPT, CHECK, COOK
 };
 
+
 class Automata {
 private:
     int cash;
+    std::unordered_map<std::string, int> drink_price;
+    std::ifstream input;
     bool ready_for_cooking;
     STATES state;
-    std::map<std::string, int> drink_price = {
-            {"coffee", 200}, {"tea", 150},
-            {"milk", 100},{"water", 50},
-            {"cola", 175}};
     std::string selected_drink;
 public:
     Automata();
+
+    ~Automata();
 
     void on();
 
@@ -28,7 +30,7 @@ public:
 
     void coin(int);
 
-    std::map<std::string, int> getMenu() const;
+    std::unordered_map<std::string, int> getMenu() const;
 
     std::string getState() const;
 
