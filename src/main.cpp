e8e7eac2dc2a8c708167f3cf39226d9ce63c1c70
Your Name <olga.frolova.2001@bk.ru>
2020-12-05 13:20:49 +0300

    new vision/wrong names of files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 //Copyright 2020 FOM
 #include "Automata.h"
 #include <iostream>
-
 using namespace std;
 
 int main() {
@@ -15,5 +14,6 @@ int main() {
     automata_1.check(0);
     automata_1.off();
     cout << automata_1.getState() << endl;
+
     return 0;
 }
