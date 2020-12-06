537ff167079f1bf186f9234433342cb7f3c38ae0
Andrew <55204554+Void-fun@users.noreply.github.com>
2020-12-06 22:16:00 +0300

    fix tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,24 +10,28 @@ TEST(Automata, test_on) {
 
 TEST(Automata, test_off) {
 	Automata automata;
+	automata.on();
 	automata.off();
 	EXPECT_EQ(OFF, automata.getState());
 }
 
 TEST(Automata, test_coin) {
 	Automata automata;
+	automata.on();
 	automata.coin(10);
 	EXPECT_EQ(ACCEPT, automata.getState());
 }
 
 TEST(Automata, test_coin_2) {
 	Automata automata;
+	automata.on();
 	automata.coin(10);
 	EXPECT_EQ(10, automata.getCash());
 }
 
 TEST(Automata, test_coin_3) {
 	Automata automata;
+	automata.on();
 	automata.coin(10);
 	automata.coin(25);
 	EXPECT_EQ(35, automata.getCash());
@@ -35,6 +39,7 @@ TEST(Automata, test_coin_3) {
 
 TEST(Automata, test_choice) {
 	Automata automata;
+	automata.on();
 	automata.coin(50);
 	automata.choice(1);
 	EXPECT_EQ(COOK, automata.getState());
@@ -42,20 +47,23 @@ TEST(Automata, test_choice) {
 
 TEST(Automata, test_choice_with_cash) {
 	Automata automata;
+	automata.on();
 	automata.coin(50);
 	automata.choice(4);
 	EXPECT_EQ(20, automata.getCash());
 }
 
-TEST(Automata, test_choice) {
+TEST(Automata, test_finish_with_error) {
 	Automata automata;
+	automata.on();
 	automata.coin(50);
 	automata.choice(3);
 	EXPECT_EQ(0, automata.getCash());
 }
 
-TEST(Automata, test_choice) {
+TEST(Automata, test_finish) {
 	Automata automata;
+	automata.on();
 	automata.coin(50);
 	automata.choice(1);
 	automata.finish();
