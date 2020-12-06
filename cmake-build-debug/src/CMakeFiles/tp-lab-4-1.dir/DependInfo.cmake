95b1c330815c5d487911ed4eafeacb9c945decf2
IgorTyulin <grizlylol@ya.ru>
2020-12-06 16:22:42 +0300

    Tyulin Igor B52

---
+++ cmake-build-debug/src/CMakeFiles/tp-lab-4-1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/progs/tp-lab-4-1/src/Automata.cpp" "E:/progs/tp-lab-4-1/cmake-build-debug/src/CMakeFiles/tp-lab-4-1.dir/Automata.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "tp_lab_4_1_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
