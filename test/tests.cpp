ba45e943b0e752e4d2613a21ac4e15be8581e192
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-12-06 20:11:39 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,23 +1,25 @@
 // Copyright 2020 test
 #include "gtest/gtest.h"
 #include "Automata.h"
+#include <string>
+#include<iostream>
 
 TEST(Constructor, test1) {
 Automata test;
-EXPECT_EQ(test.getState(), OFF);
+EXPECT_EQ(test.getState(), std::to_string(OFF));
 }
 
 TEST(STATES, test1) {
 Automata test;
 test.on();
-EXPECT_EQ(test.getState(), WAIT);
+EXPECT_EQ(test.getState(), std::to_string(WAIT));
 }
 
 TEST(STATES, test2) {
 Automata test;
 test.on();
 test.coin(50);
-EXPECT_EQ(test.getState(), ACCEPT);
+EXPECT_EQ(test.getState(), std::to_string(ACCEPT));
 }
 
 TEST(coin, test1) {
@@ -49,7 +51,7 @@ Automata test;
 test.on();
 test.coin(10);
 test.choice(1);
-EXPECT_EQ(test.getState(), WAIT);   //  Мы не смогли оплатить, деньги вернулись покупателю
+EXPECT_EQ(test.getState(), std::to_string(WAIT));   //  Мы не смогли оплатить, деньги вернулись покупателю
 }
 
 TEST(cook, test2) {
@@ -57,7 +59,7 @@ Automata test;
 test.on();
 test.coin(40);
 test.choice(1);
-EXPECT_EQ(test.getState(), WAIT);   // Покупатель получил свой товар
+EXPECT_EQ(test.getState(), std::to_string(WAIT));   // Покупатель получил свой товар
 }
 
 TEST(cook, test3) {
@@ -65,26 +67,26 @@ Automata test;
 test.on();
 test.coin(30);
 test.choice(5);
-EXPECT_EQ(test.getState(), WAIT);   // Покупатель получил свой товар
+EXPECT_EQ(test.getState(), std::to_string(WAIT));   // Покупатель получил свой товар
 }
 
 
 TEST(cancel, test1) {
 Automata test;
 test.cancel();
-EXPECT_EQ(test.getState(), OFF);    // Мы не можем отменить выключенный аппарат
+EXPECT_EQ(test.getState(), std::to_string(OFF));    // Мы не можем отменить выключенный аппарат
 }
 
 TEST(cancel, test2) {
 Automata test;
 test.on();
 test.cancel();
-EXPECT_EQ(test.getState(), WAIT);
+EXPECT_EQ(test.getState(), std::to_string(WAIT));
 }
 
 TEST(cancel, test3) {
 Automata test;
 test.on();
 test.coin(30);
-EXPECT_EQ(test.getState(), WAIT);
+EXPECT_EQ(test.getState(), std::to_string(WAIT));
 }
