d9cb6e7ce873549cd674a637c877866624b9abc2
__dolt_coder__ <ihackit595@gmail.com>
2020-12-06 20:24:56 +0300

    write tests

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -32,7 +32,6 @@ int Automata::getState() {
 
 bool Automata::choice(int position) {
     if (state == ACCEPT) {
-        // int arrayLength = ;
         if (position > 0
             && position < static_cast<int>(sizeof(menu)/sizeof(*menu))) {
             state = CHECK;
@@ -46,8 +45,10 @@ bool Automata::choice(int position) {
 
 bool Automata::check() {
     if (state == CHECK) {
-        if (cash < prices[chosenPosition])
+        if (cash < prices[chosenPosition]) {
+            state = WAIT;
             return false;
+        }
         return true;
     }
     return false;
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,85 @@
+// Copyright 2020 Bogomazov Mikhail
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(AutomataTest, test1) {
+    Automata test;
+    EXPECT_EQ(test.getState(), OFF);
+}
+
+TEST(AutomataTest, test2) {
+    Automata test;
+    test.on();
+    EXPECT_EQ(test.getState(), WAIT);
+}
+
+TEST(AutomataTest, test3) {
+    Automata test;
+    test.on();
+    test.coin(20);
+    EXPECT_EQ(test.getState(), ACCEPT);
+}
+
+
+TEST(AutomataTest, test7) {
+    Automata test;
+    test.on();
+    test.coin(12);
+    test.choice(1);
+    EXPECT_EQ(test.getState(), ACCEPT);
+}
+
+
+TEST(AutomataTest, test8) {
+    Automata test;
+    test.on();
+    test.coin(12);
+    test.choice(1);
+    test.check();
+    EXPECT_EQ(test.getState(), ACCEPT);
+}
+
+TEST(AutomataTest, test10) {
+    Automata test;
+    test.on();
+    test.coin(12);
+    test.choice(2);
+    test.check();
+    EXPECT_EQ(test.getState(), WAIT);
+}
+
+TEST(AutomataTest, test11) {
+    Automata test;
+    test.on();
+    test.coin(23);
+    test.choice(2);
+    test.check();
+    test.cook();
+    EXPECT_EQ(test.getState(), COOK);
+}
+
+TEST(AutomataTest, test12) {
+    Automata test;
+    test.on();
+    test.coin(23);
+    test.choice(2);
+    test.check();
+    test.cook();
+    test.finish();
+    EXPECT_EQ(test.getState(), WAIT);
+}
+
+TEST(AutomataTest, test13) {
+    Automata test;
+    test.on();
+    test.cook();
+    EXPECT_EQ(test.getState(), WAIT);
+}
+
+
+TEST(AutomataTest, test14) {
+    Automata test;
+    test.on();
+    test.off();
+    EXPECT_EQ(test.getState(), OFF);
+}
