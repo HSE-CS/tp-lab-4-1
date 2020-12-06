6c68acece5ceb094b040ced955eaa92f2cf7884b
sharg <jenya.sharunov@gmail.com>
2020-12-06 21:35:49 +0300

    add new test2

--- test/tests.cpp
+++ test/tests.cpp
@@ -30,19 +30,54 @@ automata.cancel();
 EXPECT_EQ(0, automata.getValue());
 }
 
-//TEST(lab4, test5)
-//{
-//Automata automata;
-//automata.on();
-//automata.coin(10);
-//automata.choice("Double Hot Chocolate");
-//automata.cancel()
-//EXPECT_EQ(1, automata.getState());
-//}
-//TEST(lab4, test6)
-//{
-//Automata automata;
-//automata.on();
-//automata.choice("Tea")
-//EXPECT_EQ(1, automata.getState());
-//}
+TEST(lab4, test5)
+{
+Automata automata;
+automata.on();
+automata.coin(10);
+automata.choice("Double Hot Chocolate");
+automata.cancel();
+EXPECT_EQ(1, automata.getState());
+}
+
+TEST(lab4, test6)
+{
+Automata automata;
+automata.on();
+automata.choice("Tea");
+EXPECT_EQ(1, automata.getState());
+}
+TEST(lab4, test7)
+{
+Automata automata;
+automata.on();
+automata.coin(80);
+automata.choice("Premium Americano");
+EXPECT_EQ(70, automata.getValue());
+}
+
+TEST(lab4, test8)
+{
+Automata automata;
+automata.on();
+automata.coin(80);
+automata.choice("Premium Americano");
+automata.coin(10);
+automata.choice("Water");
+EXPECT_EQ(90, automata.getValue());
+}
+TEST(lab4, test9)
+{
+Automata automata;
+automata.on();
+automata.coin(80);
+automata.choice("Latte");
+automata.coin(10);
+automata.cancel();
+automata.off();
+automata.choice("Water");
+EXPECT_EQ(0, automata.getState());
+}
+
+
+
