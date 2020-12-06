a356aa8cf55493b51eb5b074891ec14fa4b78212
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-12-06 20:19:34 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -19,7 +19,7 @@ TEST(STATES, test2) {
 Automata test;
 test.on();
 test.coin(50);
-EXPECT_EQ(test.getState(), std::to_string(ACCEPT));
+EXPECT_EQ(test.getState(), "ACCEPT");
 }
 
 TEST(coin, test1) {
@@ -34,7 +34,7 @@ Automata test;
 test.on();
 test.coin(10);
 test.choice(1);
-EXPECT_EQ(test.getState(), std::to_string(WAIT));   //  Мы не смогли оплатить, деньги вернулись покупателю
+EXPECT_EQ(test.getState(), "WAIT");   //  Мы не смогли оплатить, деньги вернулись покупателю
 }
 
 TEST(cook, test2) {
@@ -42,7 +42,7 @@ Automata test;
 test.on();
 test.coin(40);
 test.choice(1);
-EXPECT_EQ(test.getState(), std::to_string(WAIT));   // Покупатель получил свой товар
+EXPECT_EQ(test.getState(), "WAIT");   // Покупатель получил свой товар
 }
 
 TEST(cook, test3) {
@@ -50,26 +50,26 @@ Automata test;
 test.on();
 test.coin(30);
 test.choice(5);
-EXPECT_EQ(test.getState(), std::to_string(WAIT));   // Покупатель получил свой товар
+EXPECT_EQ(test.getState(), "WAIT");   // Покупатель получил свой товар
 }
 
 
 TEST(cancel, test1) {
 Automata test;
 test.cancel();
-EXPECT_EQ(test.getState(), std::to_string(OFF));    // Мы не можем отменить выключенный аппарат
+EXPECT_EQ(test.getState(), "OFF");    // Мы не можем отменить выключенный аппарат
 }
 
 TEST(cancel, test2) {
 Automata test;
 test.on();
 test.cancel();
-EXPECT_EQ(test.getState(), std::to_string(WAIT));
+EXPECT_EQ(test.getState(), "WAIT");
 }
 
 TEST(cancel, test3) {
 Automata test;
 test.on();
 test.coin(30);
-EXPECT_EQ(test.getState(), std::to_string(WAIT));
+EXPECT_EQ(test.getState(), "WAIT");
 }
