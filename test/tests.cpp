d6818a3ac4f6637661b039ef529cde3b85930ffd
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-12-06 20:03:21 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,27 +4,27 @@
 
 TEST(Constructor, test1) {
 Automata test;
-ASSERT_EQ(test.getState(), OFF);
+EXPECT_EQ(test.getState(), OFF);
 }
 
 TEST(STATES, test1) {
 Automata test;
 test.on();
-ASSERT_EQ(test.getState(), WAIT);
+EXPECT_EQ(test.getState(), WAIT);
 }
 
 TEST(STATES, test2) {
 Automata test;
 test.on();
 test.coin(50);
-ASSERT_EQ(test.getState(), ACCEPT);
+EXPECT_EQ(test.getState(), ACCEPT);
 }
 
 TEST(coin, test1) {
 Automata test;
 test.on();
 test.coin(500);
-ASSERT_EQ(test.getCash(), 500);
+EXPECT_EQ(test.getCash(), 500);
 }
 
 TEST(coin, test2) {
@@ -32,7 +32,7 @@ Automata test;
 test.on();
 test.coin(25);
 test.coin(4);
-ASSERT_EQ(test.getCash(), 29);
+EXPECT_EQ(test.getCash(), 29);
 }
 
 TEST(coin, test3) {
@@ -41,7 +41,7 @@ test.on();
 test.coin(146587);
 test.coin(33);
 test.coin(90);
-ASSERT_EQ(test.getCash(), 146710);
+EXPECT_EQ(test.getCash(), 146710);
 }
 
 TEST(cook, test1) {
@@ -49,7 +49,7 @@ Automata test;
 test.on();
 test.coin(10);
 test.choice(1);
-ASSERT_EQ(test.getState(), WAIT);   //  Мы не смогли оплатить, деньги вернулись покупателю
+EXPECT_EQ(test.getState(), WAIT);   //  Мы не смогли оплатить, деньги вернулись покупателю
 }
 
 TEST(cook, test2) {
@@ -57,7 +57,7 @@ Automata test;
 test.on();
 test.coin(40);
 test.choice(1);
-ASSERT_EQ(test.getState(), WAIT);   // Покупатель получил свой товар
+EXPECT_EQ(test.getState(), WAIT);   // Покупатель получил свой товар
 }
 
 TEST(cook, test3) {
@@ -65,26 +65,26 @@ Automata test;
 test.on();
 test.coin(30);
 test.choice(5);
-ASSERT_EQ(test.getState(), WAIT);   // Покупатель получил свой товар
+EXPECT_EQ(test.getState(), WAIT);   // Покупатель получил свой товар
 }
 
 
 TEST(cancel, test1) {
 Automata test;
 test.cancel();
-ASSERT_EQ(test.getState(), OFF);    // Мы не можем отменить выключенный аппарат
+EXPECT_EQ(test.getState(), OFF);    // Мы не можем отменить выключенный аппарат
 }
 
 TEST(cancel, test2) {
 Automata test;
 test.on();
 test.cancel();
-ASSERT_EQ(test.getState(), WAIT);
+EXPECT_EQ(test.getState(), WAIT);
 }
 
 TEST(cancel, test3) {
 Automata test;
 test.on();
 test.coin(30);
-ASSERT_EQ(test.getState(), WAIT);
+EXPECT_EQ(test.getState(), WAIT);
 }
