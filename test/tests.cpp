807297b5b996d7af919850bc9f6b458fe70ebfab
Andrew <55204554+Void-fun@users.noreply.github.com>
2020-12-06 22:39:39 +0300

    )))

--- test/tests.cpp
+++ test/tests.cpp
@@ -22,43 +22,12 @@ TEST(Automata, test_coin) {
 	EXPECT_EQ(ACCEPT, automata.getState());
 }
 
-TEST(Automata, test_coin_2) {
-	Automata automata;
-	automata.on();
-	automata.coin(10);
-	EXPECT_EQ(10, automata.getCash());
-}
-
-TEST(Automata, test_coin_3) {
-	Automata automata;
-	automata.on();
-	automata.coin(10);
-	automata.coin(25);
-	EXPECT_EQ(35, automata.getCash());
-}
-
 TEST(Automata, test_choice) {
 	Automata automata;
 	automata.on();
 	automata.coin(50);
 	automata.choice(1);
 	EXPECT_EQ(COOK, automata.getState());
-}
-
-TEST(Automata, test_choice_with_cash) {
-	Automata automata;
-	automata.on();
-	automata.coin(50);
-	automata.choice(4);
-	EXPECT_EQ(20, automata.getCash());
-}
-
-TEST(Automata, test_finish_with_error) {
-	Automata automata;
-	automata.on();
-	automata.coin(50);
-	automata.choice(3);
-	EXPECT_EQ(0, automata.getCash());
 }
 
 TEST(Automata, test_finish) {
