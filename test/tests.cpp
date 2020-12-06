deacbf52d461d42cc693973d5ce932f65e4c4623
Andrew <55204554+Void-fun@users.noreply.github.com>
2020-12-06 21:19:30 +0300

    tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,63 @@
+// Copyright 2020 MatveevAS
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(Automata, test_on) {
+	Automata automata;
+	automata.on();
+	EXPECT_EQ(WAIT, automata.getState());
+}
+
+TEST(Automata, test_off) {
+	Automata automata;
+	automata.off();
+	EXPECT_EQ(OFF, automata.getState());
+}
+
+TEST(Automata, test_coin) {
+	Automata automata;
+	automata.coin(10);
+	EXPECT_EQ(ACCEPT, automata.getState());
+}
+
+TEST(Automata, test_coin_2) {
+	Automata automata;
+	automata.coin(10);
+	EXPECT_EQ(10, automata.getCash());
+}
+
+TEST(Automata, test_coin_3) {
+	Automata automata;
+	automata.coin(10);
+	automata.coin(25);
+	EXPECT_EQ(35, automata.getCash());
+}
+
+TEST(Automata, test_choice) {
+	Automata automata;
+	automata.coin(50);
+	automata.choice(1);
+	EXPECT_EQ(COOK, automata.getState());
+}
+
+TEST(Automata, test_choice_with_cash) {
+	Automata automata;
+	automata.coin(50);
+	automata.choice(4);
+	EXPECT_EQ(20, automata.getCash());
+}
+
+TEST(Automata, test_choice) {
+	Automata automata;
+	automata.coin(50);
+	automata.choice(3);
+	EXPECT_EQ(0, automata.getCash());
+}
+
+TEST(Automata, test_choice) {
+	Automata automata;
+	automata.coin(50);
+	automata.choice(1);
+	automata.finish();
+	EXPECT_EQ(WAIT, automata.getState());
+}
