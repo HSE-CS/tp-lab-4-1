2feefb32b790ebf80e2de86e7136ff92007f6674
Nik <50590478+stifeevn@users.noreply.github.com>
2020-12-07 01:41:15 +0500

    fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,4 @@
-﻿#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 #include "Automata.h"
 
 TEST(lab, on_off) {
@@ -8,45 +8,45 @@ TEST(lab, on_off) {
 }
 
 TEST(lab, cash_money) {
-	Automata probe_machine = Automata();
-	probe_machine.on();	
-	probe_machine.coin(10);
-	probe_machine.coin(15);
-	EXPECT_EQ(25, probe_machine.getCash());
+	Automata probe_machine1 = Automata();
+	probe_machine1.on();	
+	probe_machine1.coin(10);
+	probe_machine1.coin(15);
+	EXPECT_EQ(25, probe_machine1.getCash());
 }
 
 TEST(lab, choice) {
-	Automata probe_machine = Automata();
-	probe_machine.on();
-	probe_machine.coin(5);
-	EXPECT_EQ(3, probe_machine.choice(6));
+	Automata probe_machine2 = Automata();
+	probe_machine2.on();
+	probe_machine2.coin(5);
+	EXPECT_EQ(3, probe_machine2.choice(6));
 
-	Automata probe_machine = Automata();
-	probe_machine.on();
-	probe_machine.coin(70);
-	EXPECT_EQ(11, probe_machine.choice(2));
+	Automata probe_machine3 = Automata();
+	probe_machine3.on();
+	probe_machine3.coin(70);
+	EXPECT_EQ(11, probe_machine3.choice(2));
 }
 
 TEST(lab4, check) {	
-	Automata probe_machine = Automata();	
-	probe_machine.on();
-	probe_machine.coin(5);
-	probe_machine.choice(1);
-	EXPECT_EQ(5, probe_machine.check());
+	Automata probe_machine4 = Automata();	
+	probe_machine4.on();
+	probe_machine4.coin(5);
+	probe_machine4.choice(1);
+	EXPECT_EQ(5, probe_machine4.check());
 
-	Automata probe_machine = Automata();
-	probe_machine.on();
-	probe_machine.coin(40);
-	probe_machine.choice(1);
-	EXPECT_EQ(11, probe_machine.check());
+	Automata probe_machine5 = Automata();
+	probe_machine5.on();
+	probe_machine5.coin(40);
+	probe_machine5.choice(1);
+	EXPECT_EQ(11, probe_machine5.check());
 }
 
 TEST(lab4, cook) {
-	Automata probe_machine = Automata();
-	probe_machine.on();
-	EXPECT_EQ(4, probe_machine.cook());
-	probe_machine.coin(200);
-	probe_machine.choice(1);
-	probe_machine.check();
-	EXPECT_EQ(11, probe_machine.cook());
+	Automata probe_machine6 = Automata();
+	probe_machine6.on();
+	EXPECT_EQ(4, probe_machine6.cook());
+	probe_machine6.coin(200);
+	probe_machine6.choice(1);
+	probe_machine6.check();
+	EXPECT_EQ(11, probe_machine6.cook());
 }
