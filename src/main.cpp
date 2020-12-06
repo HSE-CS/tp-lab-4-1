1d0c90a72a98503b0fa37eab1771ce5484916be6
stifeevn <stifeev.nikita@mail.ru>
2020-12-07 01:24:11 +0500

    lab4

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright 2020 Nikita Stifeev
+#include "Automata.h"
+
+int main() {
+	Automata my_machine = Automata();
+	my_machine.on();
+	std::cout << "Your budget:" << std::endl;
+	int budget;
+	std::cin >> budget;
+	my_machine.coin(budget);
+	int n;
+	std::cout << "what do you want to buy?" << std::endl;
+	std::cin >> n;
+	if (my_machine.choice(n) != 11) {
+		my_machine.cancel();
+	}
+	else if (my_machine.check() == 11) {
+		my_machine.cook();
+		my_machine.finish();
+	}
+	else {
+		my_machine.cancel();
+	}
+	my_machine.off();
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,52 @@
-#include <gtest/gtest.h>
+﻿#include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(lab, on_off) {
+	CoffeeMachine probe = Automata();
+	EXPECT_EQ(11, probe.on());
+	EXPECT_EQ(11, probe.off());
+}
+
+TEST(lab, cash_money) {
+	CoffeeMachine probe_machine = Automata();
+	probe_machine.on();	
+	probe_machine.coin(10);
+	probe_machine.coin(15);
+	EXPECT_EQ(25, probe_machine.getCash());
+}
+
+TEST(lab, choice) {
+	Automata probe_machine = Automata();
+	probe_machine.on();
+	probe_machine.coin(5);
+	EXPECT_EQ(3, probe_machine.choice(6));
+
+	Automata probe_machine = Automata();
+	probe_machine.on();
+	probe_machine.coin(70);
+	EXPECT_EQ(11, probe_machine.choice(2));
+}
+
+TEST(lab4, check) {	
+	Automata probe_machine = Automata();	
+	probe_machine.on();
+	probe_machine.coin(5);
+	probe_machine.choice(1);
+	EXPECT_EQ(5, probe_machine.check());
+
+	Automata probe_machine = Automata();
+	probe_machine.on();
+	probe_machine.coin(40);
+	probe_machine.choice(1);
+	EXPECT_EQ(11, probe_machine.check());
+}
+
+TEST(lab4, cook) {
+	Automata Igor = Automata();
+	probe_machine.on();
+	EXPECT_EQ(4, probe_machine.cook());
+	probe_machine.coin(200);
+	probe_machine.choice(1);
+	probe_machine.check();
+	EXPECT_EQ(11, Igor.cook());
+}
