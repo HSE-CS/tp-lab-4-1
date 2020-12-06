90aa458e3aa63b0813300ec677826f02b9b1c6f1
DenisKabanov <dskabanov@edu.hse.ru>
2020-12-06 17:37:45 +0300

    more tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main() 
 {
+	cout << "test 1:" << endl;
 	Automata a;
 	cout << "State= " << a.getState() << endl;
 	a.on();
@@ -13,5 +14,32 @@ int main()
 	a.getMenu();
 	a.choice(5);
 	cout << "State= " << a.getState() << endl;
+	cout << endl;
+
+	cout << "test 2:" << endl;
+	Automata b;
+	b.on();
+	b.coin(998, true);
+	cout << "State= " << b.getState() << endl;
+	b.choice(7);
+	cout << "State= " << b.getState() << endl;
+	cout << endl;
+
+	cout << "test 3:" << endl;
+	Automata c;
+	c.on();
+	c.coin(100, true);
+	cout << "State= " << c.getState() << endl;
+	c.choice(1);
+	cout << "State= " << c.getState() << endl;
+	c.coin(100, true);
+	cout << "State= " << c.getState() << endl;
+	c.choice(2);
+	cout << "State= " << c.getState() << endl;
+	c.coin(100, true);
+	cout << "State= " << c.getState() << endl;
+	c.choice(3);
+	cout << "State= " << c.getState() << endl;
+
 	return 0;
 }
