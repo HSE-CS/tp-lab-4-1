9732073308d380e5d66a04b220adc26069521589
EgorBuzanov <egor4ek007@gmail.com>
2020-12-06 18:10:52 +0300

    Egor Buzanov

---
+++ src/Automata.cpp
@@ -0,0 +1,62 @@
+// Copyright 2020 Egor Buzanov
+#include "../include/Automata.h"
+
+const std::map<std::string, int> MENU = {
+    {"Americano", 25}, {"Cappuccino", 30}, {"Tea", 15}, {"Lemon tea", 20}};
+
+Automata::Automata() {
+  cash = 0;
+  state = OFF;
+  menu = MENU;
+  product = "";
+}
+
+void Automata::on() { state = WAIT; }
+
+void Automata::off() { state = OFF; }
+
+void Automata::coin(unsigned int coin) {
+  if (state == WAIT || state == ACCEPT) {
+    state == ACCEPT;
+    cash += coin;
+  }
+}
+
+std::map<std::string, int> Automata::getMenu() { return menu; }
+
+STATES Automata::getState() { return state; }
+
+void Automata::choice(const std::string chosen) {
+  if (state == ACCEPT) {
+    product = chosen;
+  }
+}
+
+void Automata::check() {
+  if (state == ACCEPT) {
+    if (cash >= menu[product]) {
+      state = CHECK;
+    } else
+      state = ACCEPT;
+  }
+}
+
+void Automata::cancel() {
+  state = WAIT;
+  cash = 0;
+  product = "";
+}
+
+void Automata::cook() {
+  if (state == CHECK) {
+    state = COOK;
+  }
+}
+
+void Automata::finish() {
+  if (state == COOK) {
+    state = WAIT;
+    product = "";
+    cash = 0;
+  }
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,73 @@
+// Copyright 2020 Egor Buzanov
 #include <gtest/gtest.h>
+
 #include "Automata.h"
+
+const std::map<std::string, int> MENU = {
+    {"Americano", 25}, {"Cappuccino", 30}, {"Tea", 15}, {"Lemon tea", 20}};
+
+TEST(automata, off1) {
+  Automata coffee_machine;
+  EXPECT_EQ(OFF, coffee_machine.getState());
+}
+
+TEST(automata, on) {
+  Automata coffee_machine;
+  coffee_machine.on();
+  EXPECT_EQ(WAIT, coffee_machine.getState());
+}
+
+TEST(automata, menu) {
+  Automata coffee_machine;
+  coffee_machine.on();
+  EXPECT_EQ(MENU, coffee_machine.getMenu());
+}
+
+TEST(automata, coin) {
+  Automata coffee_machine;
+  coffee_machine.on();
+  coffee_machine.coin(10);
+  EXPECT_EQ(ACCEPT, coffee_machine.getState());
+}
+
+TEST(automata, check) {
+  Automata coffee_machine;
+  coffee_machine.on();
+  coffee_machine.coin(20);
+  coffee_machine.choice("Lemon tea");
+  coffee_machine.check();
+  EXPECT_EQ(CHECK, coffee_machine.getState());
+}
+
+TEST(automata, cook) {
+  Automata coffee_machine;
+  coffee_machine.on();
+  coffee_machine.coin(20);
+  coffee_machine.choice("Lemon tea");
+  coffee_machine.check();
+  coffee_machine.cook();
+  EXPECT_EQ(COOK, coffee_machine.getState());
+}
+
+TEST(automata, finish) {
+  Automata coffee_machine;
+  coffee_machine.on();
+  coffee_machine.coin(20);
+  coffee_machine.choice("Lemon tea");
+  coffee_machine.check();
+  coffee_machine.cook();
+  coffee_machine.finish();
+  EXPECT_EQ(WAIT, coffee_machine.getState());
+}
+
+TEST(automata, off2) {
+  Automata coffee_machine;
+  coffee_machine.on();
+  coffee_machine.coin(20);
+  coffee_machine.choice("Lemon tea");
+  coffee_machine.check();
+  coffee_machine.cook();
+  coffee_machine.finish();
+  coffee_machine.off();
+  EXPECT_EQ(OFF, coffee_machine.getState());
+}
