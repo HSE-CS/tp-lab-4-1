a4d9e4e117a37f5c05b0528e03a97b6bdbd8477d
ch2oh-ch2oh <valerasofronov01@gmail.com>
2020-12-08 14:55:34 +0300

    first commit

---
+++ include/Automata.h
@@ -0,0 +1,33 @@
+// Copyright 2020 aaa
+
+#ifndef AUTOMATA_H
+#define AUTOMATA_H
+
+#include <iostream>
+#include <string>
+
+enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
+
+class Automata {
+private:
+    double cash;
+    const static int menuSize=8;
+    std::string menu[menuSize] = {"hot beer", "tea", "milk", "coffee", "americano", "espresso", "hot chocolate", "hot water"};
+    double prices[menuSize] = {1.0, 2.9, 3.8, 4.7, 5.6, 6.5, 7.4, 8.3};
+    STATES state;
+public:
+    Automata() {cash = 0; state = OFF;}
+    void on();
+    void off();
+    void coin(double);
+    std::string getMenu();
+    STATES getState();
+    void choice(int);
+    void check(int);
+    void cancel();
+    void cook(int);
+    void finish(int);
+};
+
+
+#endif //AUTOMATA_H
---
+++ src/Automata.cpp
@@ -0,0 +1,62 @@
+// Copyright 2020 aaa
+#include "Automata.h"
+
+std::string Automata::getMenu() {
+    std::string table;
+    for (int i = 0; i < menuSize; ++i) {
+        table += std::to_string(i+1) + ".\t" + menu[i] + "\t" + std::to_string(prices[i]) + " coins\n";
+    }
+    return table;
+}
+
+void Automata::coin(double amount) {
+    state = STATES::ACCEPT;
+    cash += amount;
+    std::cout << "Coins accepted" << std::endl;
+}
+
+void Automata::off() {
+    state = OFF;
+    std::cout << "Automata turned off" << std::endl;
+}
+
+void Automata::on() {
+    state = WAIT;
+    std::cout << "Insert coins" << std::endl;
+}
+
+STATES Automata::getState() {
+    return state;
+}
+
+void Automata::choice(int beverage) {
+    state = CHECK;
+    check(beverage);
+}
+
+void Automata::check(int number) {
+    if (cash >= prices[number]) {
+        cook(number);
+        return;
+    }
+    std::cout << "Not enough coins" << std::endl;
+    state = ACCEPT;
+}
+
+void Automata::cancel() {
+    state = WAIT;
+    cash = 0;
+    std::cout << "Canceled" << std::endl;
+}
+
+void Automata::cook(int beverage) {
+    state = COOK;
+    std::cout << "Beverage is cooking" << std::endl;
+    finish(beverage);
+}
+
+void Automata::finish(int beverage) {
+    std::cout << "Get rest of money: " << cash - prices[beverage] << std::endl;
+    cash = 0;
+    state = WAIT;
+}
