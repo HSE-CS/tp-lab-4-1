707bab3fe958291f29d8a89be92fcbd9fde62fad
ch2oh-ch2oh <valerasofronov01@gmail.com>
2020-12-08 16:20:09 +0300

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,16 @@
 #include "Automata.h"
 
 int main(){
-
+    Automata a;
+    a.on();
+    std::cout << a.getMenu();
+    a.coin(10);
+    a.coin(5);
+    a.choice(3);
+    std::cout << a.getCash() << std::endl;
+    a.coin(2);
+    a.choice(6);
+    a.coin(10);
+    a.choice(6);
+    a.off();
 }
