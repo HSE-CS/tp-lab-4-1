f68b9ab38c455f00af73d655435a6d02df595df9
Stolbov Yaroslav <stojar2001@gmail.com>
2020-12-17 11:23:53 +0300

    hz

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,14 @@
 #include <iostream>
 
 int main(){
-    /*Automata automata = Automata();
+    Automata automata = Automata();
     automata.on();
     automata.coin(50);
     automata.coin(100);
     std::cout<<automata.getCash()<<"\n";
     automata.choice(5);
-    automata.choice(1);*/
-    Automata automata = Automata();
-    automata.on();
-    automata.coin(50);
-    automata.coin(100);
     automata.choice(1);
-    std::cout<<automata.getState();
+
     return 0;
 }
 
