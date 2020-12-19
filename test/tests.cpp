390be102f5c75597d297aaf2383d0820232ab572
Ramazan <ramazankka220@gmail.com>
2020-12-20 04:22:56 +0500

    i believe i can do that for the third time

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,61 +1,39 @@
+#include <gtest/gtest.h>
 #include "Automata.h"
-#include "gtest/gtest.h"
 
-TEST(automata, test1) {
-	Automata tst;
-	EXPECT_EQ(OFF, tst.getState());
-}
-
-TEST(automata, test2) {
+TEST(Automata, test1) {
 	Automata tst;
 	tst.on();
 	EXPECT_EQ(WAIT, tst.getState());
 }
 
-TEST(automata, test3) {
+TEST(Automata, test2) {
 	Automata tst;
+	tst.on();
 	tst.off();
 	EXPECT_EQ(OFF, tst.getState());
 }
 
-TEST(automata, test4) {
+TEST(Automata, test3) {
 	Automata tst;
 	tst.on();
-	tst.coin(10);
+	tst.coin(123);
 	EXPECT_EQ(ACCEPT, tst.getState());
 }
 
-TEST(automata, test5) {
+TEST(Automata, test4) {
 	Automata tst;
 	tst.on();
-	tst.coin(20);
-	tst.choice("tea");
-	EXPECT_EQ(CHECK, tst.getState());
-}
-
-TEST(automata, test6) {
-	Automata tst;
-	tst.on();
-	tst.coin(10);
-	tst.choice("");
-	tst.cook();
+	tst.coin(300);
+	tst.choice(1);
 	EXPECT_EQ(COOK, tst.getState());
 }
 
-TEST(automata, test7) {
+TEST(Automata, test5) {
 	Automata tst;
 	tst.on();
-	tst.coin(30);
-	tst.choice("Capuccino");
-	EXPECT_TRUE(tst.check());
+	tst.coin(50);
+	tst.choice(3);
+	tst.finish();
+	EXPECT_EQ(WAIT, tst.getState());
 }
-
-TEST(automata, test8) {
-	Automata tst;
-	tst.on();
-	tst.coin(100);
-	tst.choice("Kumis");
-	EXPECT_FALSE(tst.check());
-}
-
-
